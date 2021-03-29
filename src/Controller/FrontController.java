c312ba697967e18338f07d8fe8c14d26441fb47c
김승연 <57320841+ksygt728@users.noreply.github.com>
2021-03-29 15:39:24 +0900

    03.29 made MVC pattern

---
+++ src/Controller/FrontController.java
@@ -0,0 +1,43 @@
+package Controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FrontController
+ */
+@WebServlet("*.do")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		request.setCharacterEncoding("UTF-8");
+		
+		String viewPage = null;
+		String URI = request.getRequestURI(); // /MidtermTest/view.do
+		String contextPath = request.getContextPath(); // /MidtermTest
+		String com = URI.substring(contextPath.length()); //  /view.do
+	
+		if (com.equals("/index.do")) {
+			viewPage = "index.jsp";
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response); //RequestDispatcher 현재 req와 res객체를 공유한다.
+	}
+}
