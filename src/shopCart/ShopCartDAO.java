ab6c80f9d12ca6e7edb68fecde02634d21c467c4
김승연 <57320841+ksygt728@users.noreply.github.com>
2021-04-07 19:34:02 +0900

    2021.04.07 item구매시 장바구니 목록에 추가 된 상품이라면 장바구니에서 삭제되도록 수정

--- src/shopCart/ShopCartDAO.java
+++ src/shopCart/ShopCartDAO.java
@@ -21,7 +21,7 @@ public class ShopCartDAO {
 //			String dbURL = "jdbc:mysql://localhost:3306/shopping_midtermtest";
 //			String dbID = "root";
 //			String dbPassword = "";
-			
+
 			Class.forName("com.mysql.jdbc.Driver");
 			DBUser DBuser = new DBUser();
 
@@ -31,10 +31,9 @@ public class ShopCartDAO {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public boolean checkItem_In_ShopCart(String ID, int itemCode) {
 		String SQL = "SELECT * FROM shopping_cart WHERE ID = ? AND itemCode = ?";
-
 
 		try {
 			pstmt = conn.prepareStatement(SQL);
@@ -51,21 +50,18 @@ public class ShopCartDAO {
 		return true;
 	}
 
-	
-	
 	public int addShopCart(int itemCode, String ID, String folderName) {
 		String SQL = "INSERT INTO shopping_cart VALUES(?,?,NOW(),?)";
-		
+
 		try {
-			if (checkItem_In_ShopCart(ID, itemCode)) { //장바구니에 중복된 상품이 없으면
+			if (checkItem_In_ShopCart(ID, itemCode)) { // 장바구니에 중복된 상품이 없으면
 				pstmt = conn.prepareStatement(SQL);
 				pstmt.setInt(1, itemCode);
 				pstmt.setString(2, ID);
 				pstmt.setString(3, "folderName"); // 일단 예시(추후 수정 필요 할 수도 있음)
 				return pstmt.executeUpdate();
-			}
-			else {
-				return -1; //이미 추가한 상품
+			} else {
+				return -1; // 이미 추가한 상품
 			}
 
 		} catch (Exception e) {
@@ -91,8 +87,8 @@ public class ShopCartDAO {
 				ShopCartDTO shopCartDto = new ShopCartDTO();
 				shopCartDto.setItemCode(rs.getInt(1));
 				shopCartDto.setID(rs.getString(2));
-				shopCartDto.setAddDate(rs.getString(3).substring(0, 4) + "." + rs.getString(3).substring(5, 7)
-						+ "." + rs.getString(3).substring(8, 10));
+				shopCartDto.setAddDate(rs.getString(3).substring(0, 4) + "." + rs.getString(3).substring(5, 7) + "."
+						+ rs.getString(3).substring(8, 10));
 				shopCartDto.setFolderName(rs.getString(4));
 				list.add(shopCartDto);
 			}
@@ -102,7 +98,7 @@ public class ShopCartDAO {
 		return list;
 	}
 
-	//날짜 별로 for문을 돌리기 위해 날짜만 이용해서 group형식으로 가져옴
+	// 날짜 별로 for문을 돌리기 위해 날짜만 이용해서 group형식으로 가져옴
 	public ArrayList<String> getAddDate(String ID) {
 		String SQL = "SELECT substr(addDate,1,10) FROM shopping_cart WHERE ID = ? GROUP BY substr(addDate,1,10) ORDER BY addDate DESC";
 		ArrayList<String> list = new ArrayList<>();
@@ -120,6 +116,25 @@ public class ShopCartDAO {
 		}
 		return list;
 	}
-	
-	
+
+	public int deletePurchaseItem_In_ShopCart(String ID, int itemCode) {
+		String SQL = "DELETE FROM shopping_cart WHERE ID = ? AND itemCode = ?";
+		try {
+			if (!checkItem_In_ShopCart(ID, itemCode)) { // 장바구니에 추가한 상품이 있다면
+				pstmt = conn.prepareStatement(SQL);
+				pstmt.setString(1, ID);
+				pstmt.setInt(2, itemCode);
+				return pstmt.executeUpdate();
+			}
+			else {
+				return 0; //삭제안하고 넘어감
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return -1; // DB오류
+
+	}
+
 }
