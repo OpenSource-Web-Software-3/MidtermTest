ded83bcc56a0c1a62b676d9b9f12ce1b574ec84c
yu-podong <yu01gun@naver.com>
2021-04-15 05:10:35 +0900

    2021.04.15 반응형웹을 위한 작업

--- WebContent/Web-source/js/moblie-mode.js
+++ WebContent/Web-source/js/moblie-mode.js
@@ -1,6 +1,23 @@
 const moreRightBtn = document.querySelector('.more-right-btn');
 const rightBanner = document.querySelector('.right-banner');
 
+const navbarBtn = document.querySelector('.navbar .fa-bars');
+const navbar = document.querySelector('.navbar .set-margin');
+
+const asideBtn = document.querySelector('.asideBtn');
+const moblieAside = document.querySelector('.category-aside');
+const background = document.querySelector('.background');
+
+
 moreRightBtn.addEventListener('click', () => {
 	rightBanner.classList.toggle("active");
+});
+
+navbarBtn.addEventListener('click', () => {
+	navbar.classList.toggle("active");
+});
+
+asideBtn.addEventListener('click', () => {
+	moblieAside.classList.toggle("active");
+	background.classList.toggle("active");
 });
